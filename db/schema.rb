e2a713836f47337665c04a51a2ae4d647faeff04
Epharus Ndumia <epharusndumia@gmail.com>
2023-07-05 22:53:50 +0300

    migrated profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_05_165139) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_05_194705) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -34,6 +34,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_05_165139) do
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_profiles_on_email"
+    t.index ["username"], name: "index_profiles_on_username"
   end
 
   create_table "users", force: :cascade do |t|
