b1427cd76a0580bd9d4fe409752330167be5b7fb
fascilio <fascilior@gmail.com>
2023-07-06 19:37:46 +0300

    updated sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,8 @@
 class SessionsController < ApplicationController
+  # skip_before_action :verify_authenticity_token, only: [:create, :authenticate]
+    # before_action :require_login, except: [:login, :authenticate]
+    # protect_from_forgery with: :exception, except: [:authenticate]
+
     def create
       # user = User.new(username: params[:username], password: params[:password])
   
@@ -14,12 +18,18 @@ class SessionsController < ApplicationController
         session[:user_id] = user.id
         render :login
       else
-        render json: { error: 'Invalid username, email, or password' }, status: :unauthorized
+        render json: { error: 'Invalid email, or password' }, status: :unauthorized
       end
     end
   
     def destroy
       session.delete(:user_id)
       head :no_content
+    end
+
+    private
+
+    def authenticate
+      
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
-    skip_before_action :require_login, except: [:logout]
-    skip_before_action :verify_authenticity_token, only: [:create, :authenticate]
+    #  skip_before_action :require_login, except: [:create, :logout]
+rescue_from ActiveRecord::RecordInvalid, with: :invalid_response
+    # skip_before_action :verify_authenticity_token, only: [:create, :authenticate]
     # before_action :require_login, except: [:login, :authenticate]
-    protect_from_forgery with: :exception, except: [:authenticate]
-    before_action :require_login
+    # protect_from_forgery with: :exception, except: [:authenticate]
+    # # before_action :require_login
 
     def index
         users = User.all
@@ -15,25 +16,21 @@ class UsersController < ApplicationController
       render json: user
     end
   
-    # def create
-    #   user = User.new(user_params)
-  
-    #   if user.save
-    #     session[:user_id] = user.id
-    #     render json: user
-    #   else
-    #     render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
-    #   end
-    # end
     def create
-        user = User.new(user_params)
+      user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user
     
-        if user.save
-          redirect_to articles_path
-        else
-          redirect_to signup_path
-        end
     end
+    # def create
+    #     user = User.new(user_params)
+    
+    #     if user.save
+    #       redirect_to articles_path
+    #     else
+    #       redirect_to signup_path
+    #     end
+    # end
   
     # def login
     #     render :login
@@ -46,16 +43,16 @@ class UsersController < ApplicationController
     #     end
     # end
   
-    # def authenticate
-    #   user = User.find_by(email: params[:email], password: params[:password])
+    def authenticate
+      user = User.find_by(email: params[:email])
   
-    #   if user && user.authenticate(params[:password])
-    #     session[:user_id] = user.id
-    #     render :login
-    #   else
-    #     render json: { error: 'Invalid username, email, or password' }, status: :unauthorized
-    #   end
-    # end
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        render :login
+      else
+        render json: { error: 'Invalid username, email, or password' }, status: :unauthorized
+      end
+    end
   
     def logout
       session.delete[:user_id] 
@@ -72,6 +69,11 @@ class UsersController < ApplicationController
         unless current_user
           redirect_to login_path, alert: "Please log in to continue."
         end
+    end
+
+    def invalid_response(invalid)
+      render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+      
     end
   end
 # class UsersController < ApplicationController
