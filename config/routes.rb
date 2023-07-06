a42ff2234ded36060b527ed7ca10af17cefa72f6
Epharus Ndumia <epharusndumia@gmail.com>
2023-07-06 17:16:33 +0300

    added profile routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   get '/login', to: 'users#login'
   post '/login', to: 'users#authenticate'
   get '/logout', to: 'users#logout'
-  
+  get '/profiles', to: 'profiles#index'
+  get '/profiles/:id', to: 'profiles#show'
+  post '/profiles', to: 'profiles#create'
+  patch '/profiles/:id', to: 'profiles#update'
+  delete '/profiles/:id', to: 'profiles#destroy'
 
 end
